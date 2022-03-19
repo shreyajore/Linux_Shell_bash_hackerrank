d02c972222acca283b0cf44029f22f24e09303fd
shreyajore <79442971+shreyajore@users.noreply.github.com>
2022-03-19 23:01:49 +0530

    Create theWorldofNumbers.sh

---
+++ theWorldofNumbers.sh
@@ -0,0 +1,9 @@
+# Given two integers, finds their sum, difference, product and quotient.
+
+read X
+read Y
+
+echo "$(( $X + $Y ))"
+echo "$(( $X - $Y ))"
+echo "$(( $X * $Y ))"
+echo "$(( $X / $Y ))"
